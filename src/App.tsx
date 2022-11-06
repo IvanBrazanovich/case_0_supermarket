e955f35769fb09c223007d6bb5537476f60bf632
Ivan <ivanbrazas@gmail.com>
2022-11-06 10:09:20 -0300

    Finished

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,29 @@ import styles from "./App.module.scss";
 import api from "./api";
 
 function App() {
-  const [items, setItems] = useState<Item[] | null>(null);
+  const [items, setItems] = useState<Item[]>([]);
 
   useEffect(() => {
     api.list().then(setItems);
   }, []);
 
+
+  const handleEliminar =(id)  => {
+    const newArr = items.filter(item => item.id !== id) 
+    console.log(newArr)
+    setItems(newArr)
+  }
+
   return (
     <main className={styles.main}>
       <h1>Supermarket list</h1>
       <form>
-        <input name="text" type="text" />
+        <input autoFocus name="text" type="text" />
         <button>Add</button>
       </form>
       <ul>
-        {items.map((item) => (
-          <li className={item.completed ? styles.completed : ""}>
+        {items?.map((item) => (
+          <li key={item.id} onClick={() => handleEliminar(item.id)}  className={item.completed ? styles.completed : ""}>
             {item.text} <button>[X]</button>
           </li>
         ))}
